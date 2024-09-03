f970ea96d23973ef2db52c59f382cfd75a69afd6
anandu20 <ananduramachandran411@gmail.com>
2024-09-03 09:30:40 +0530

    node

---
+++ serverside/server.js
@@ -0,0 +1,62 @@
+const http=require("http");
+const fs=require("fs");
+const url=require("url");
+const queryString=require("querystring");
+const {MongoClient}=require("mongodb");
+//connect database
+const client =new MongoClient()
+const app=http.createServer((req,res)=>{
+    const path=url.parse(req.url);
+    console.log(path);
+    console.log(req.method);
+
+    if (path.pathname=="/"){
+        res.writeHead(200,{"content-type":"text/html"});
+        res.end(fs.readFileSync("../clientside/index.html"));
+    }
+    else if(path.pathname=="/clientside/js/custom.js"){
+
+        res.writeHead(200,{"content-type":"text/html"});
+        res.end(fs.readFileSync("../clientside/js/custom.js"));
+
+    }
+    else if(path.pathname=="/adddonar"){
+
+        res.writeHead(200,{"content-type":"text/html"});
+        res.end(fs.readFileSync("../clientside/pages/add.html"));
+    
+    }
+
+    //fetch data from donor
+
+if(path.pathname=="/submit" && req.method=="POST"){
+    let body="";
+    req.on("data",(chunks)=>{
+        body+=chunks.toString();
+        console.log(body);
+
+    });
+    req.on("end",async()=>{
+        //convert query strng to object 
+        const formData=queryString.parse(body)
+        console.log(formData);
+        //insert into collection
+        collection.insertOne(formData).then(()=>{
+            console.log("success");
+            
+
+        })
+        .catch((error)=>{
+            console.log(error);
+            
+        })
+        
+    });
+    res.writeHead(200,{"content-type":"text/html"});
+    res.end(fs.readFileSync("../clientside/index.html"));
+}
+
+});
+
+
+app.listen(3000);
