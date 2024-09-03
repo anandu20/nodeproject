79a26479f457ff7b88d3c615c6ad071e2a5d926f
anandu20 <ananduramachandran411@gmail.com>
2024-09-03 20:23:26 +0530

    node

--- serverside/server.js
+++ serverside/server.js
@@ -4,25 +4,31 @@ const url=require("url");
 const queryString=require("querystring");
 const {MongoClient}=require("mongodb");
 //connect database
-const client =new MongoClient()
+const client =new MongoClient("mongodb://127.0.0.1:27017//");
 const app=http.createServer((req,res)=>{
+
+    //create database
+    const db=client.db("Blood_bank");
+    //create collections
+    const collection=db.collection("donors");
+
     const path=url.parse(req.url);
     console.log(path);
     console.log(req.method);
 
     if (path.pathname=="/"){
-        res.writeHead(200,{"content-type":"text/html"});
+        res.writeHead(200,{"Content-type":"text/html"});
         res.end(fs.readFileSync("../clientside/index.html"));
     }
     else if(path.pathname=="/clientside/js/custom.js"){
 
-        res.writeHead(200,{"content-type":"text/html"});
+        res.writeHead(200,{"Content-type":"text/html"});
         res.end(fs.readFileSync("../clientside/js/custom.js"));
 
     }
     else if(path.pathname=="/adddonar"){
 
-        res.writeHead(200,{"content-type":"text/html"});
+        res.writeHead(200,{"Content-type":"text/html"});
         res.end(fs.readFileSync("../clientside/pages/add.html"));
     
     }
@@ -49,14 +55,15 @@ if(path.pathname=="/submit" && req.method=="POST"){
         .catch((error)=>{
             console.log(error);
             
-        })
+        });
+        res.writeHead(200,{"Content-type":"text/html"});
+        res.end(fs.readFileSync("../clientside/index.html"));
         
     });
-    res.writeHead(200,{"content-type":"text/html"});
-    res.end(fs.readFileSync("../clientside/index.html"));
-}
+ 
+    }
 
-});
+ });
 
 
 app.listen(3000);
